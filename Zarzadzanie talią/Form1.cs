f9fba6a53627ff4e91a652ba50bcaa12c9ede83a
SiwyKrzysiek <k.fajny@gmail.com>
2017-08-31 13:22:19 +0200

    Reset Deck and Redraw Deck metods added

--- Zarzadzanie talią/Form1.cs
+++ Zarzadzanie talią/Form1.cs
@@ -12,9 +12,46 @@ namespace Zarzadzanie_talią
 {
     public partial class Form1 : Form
     {
+        Deck deck1, deck2;
+        Random random = new Random();
+
         public Form1()
         {
             InitializeComponent();
+            
+        }
+
+        void ResetDeck(int deckNumber)
+        {
+            if (deckNumber == 1)
+            {
+                int numberOfCards = random.Next(1, 11);
+                deck1 = new Deck(new Card[] { });
+                for (int i = 0; i < numberOfCards; i++)
+                {
+                    deck1.Add(new Card());
+                }
+
+                deck1.Sort();
+            }
+            if (deckNumber == 2)
+                deck2 = new Deck();
+        }
+
+        void RedrawDeck(int deckNumber)
+        {
+            if (deckNumber == 1)
+            {
+                listBox1.Items.Clear();
+                foreach (string cardName in deck1.GetCardNames())
+                    listBox1.Items.Add(cardName);
+            }
+            if (deckNumber == 2)
+            {
+                listBox2.Items.Clear();
+                foreach (string cardName in deck2.GetCardNames())
+                    listBox2.Items.Add(cardName);
+            }
         }
     }
 }
